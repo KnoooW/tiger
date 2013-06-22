570ae48d5b707dd969a05eb58fb6cb4b40650eb1
KnoooW <KnoooW@126.com>
2013-06-22 16:08:45 +0800

    lab1_finish

--- test/QuickSort.java
+++ test/QuickSort.java
@@ -20,7 +20,7 @@ class QS{
 	aux01 = this.Init(sz);
 	aux01 = this.Print();
 	System.out.println(9999);
-	aux01 = size - 1 ;
+	aux01 = size - 1 ; 
 	aux01 = this.Sort(0,aux01);
 	aux01 = this.Print();
 	return 0 ;
@@ -105,7 +105,7 @@ class QS{
 	number[7] = 9  ; 
 	number[8] = 19 ; 
 	number[9] = 5  ;
-	
+	 
 	return 0 ;	
     }
 
